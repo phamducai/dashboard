285be4b125fe6e043ce17fdd44d7952c4fb1feb2
phamducai <phamducaihcmut2009@gmail.com>
2024-07-10 18:37:53 +0700

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -2,11 +2,10 @@ import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn:'/login',
   },
   providers: [
-    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
-    // while this file is also used in non-Node.js environments
+    // Your providers here
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
